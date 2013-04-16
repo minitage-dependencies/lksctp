8209a1af84ad9793749f8cca5dfb138301cb468c
kiorky <kiorky@cryptelium.net>
2013-04-16 09:34:56 +0200

    erlang

--- h.py
+++ h.py
@@ -5,7 +5,6 @@ __docformat__ = 'restructuredtext en'
 import os
 
 def p(op, bu):
-    import pdb;pdb.set_trace()  ## Breakpoint ##
     os.system('chmod +x %s' % op['compile-directory']+'/configure')
 
 # vim:set et sts=4 ts=4 tw=80:
